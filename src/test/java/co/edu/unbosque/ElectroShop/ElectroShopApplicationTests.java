f1a70b623de9360885537a28d06810ba40219b75
Exiic <122654327+3xiic@users.noreply.github.com>
2024-08-27 16:47:57 -0500

    2 Upload

---
+++ src/test/java/co/edu/unbosque/ElectroShop/ElectroShopApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.ElectroShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
