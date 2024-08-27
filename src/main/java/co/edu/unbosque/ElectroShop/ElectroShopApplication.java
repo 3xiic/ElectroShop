c60e575ed5da0bab7e7c05ed128ab45e69a3a6bf
Miguel Linares <107733760+MikaNiatsu@users.noreply.github.com>
2024-08-27 17:21:30 -0500

    Include Swagger and Folder struct

---
+++ src/main/java/co/edu/unbosque/ElectroShop/ElectroShopApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.ElectroShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectroShopApplication.class, args);
+	}
+
+}
