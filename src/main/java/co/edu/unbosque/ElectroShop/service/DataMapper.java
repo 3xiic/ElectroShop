6d9f59b7c3e52f2604a28ce5769f9f6608865fe7
Exiic <122654327+3xiic@users.noreply.github.com>
2024-09-03 15:19:26 -0600

    DataMapper complete

--- src/main/java/co/edu/unbosque/ElectroShop/service/DataMapper.java
+++ src/main/java/co/edu/unbosque/ElectroShop/service/DataMapper.java
@@ -31,29 +31,53 @@ public class DataMapper {
 	
 	public static Category categoryDTOToCategory(CategoryDTO categoryDTO) {
 		Category category  = new Category();
+		Set<Product> products = null;
 		category.setCategory_id(categoryDTO.getCategory_id());
 		category.setCategory_name(categoryDTO.getCategory_name());
 		category.setCategory_description(categoryDTO.getCategory_description());
+		category.setCategory_description(categoryDTO.getCategory_description());
+		
+		for (ProductDTO productDTO : categoryDTO.getProducts())	{
+			products.add(productDTOToProduct(productDTO));
+		}
+		
+		category.setProducts(products);
 		return category;
 	}
 	
 	public static ClientDTO clientToClientDTO( Client client) {
 		ClientDTO clientDTO = new ClientDTO();
+		Set<OrderDTO> orderDTOs = null;
 		clientDTO.setClient_id(client.getClient_id());
 		clientDTO.setClient_name(client.getClient_name());
 		clientDTO.setClient_lastname(client.getClient_lastname());
 		clientDTO.setAddress(client.getAddress());
 		clientDTO.setPhone_number(client.getPhone_number());
+		
+		for (Order order : client.getOrders()) {
+			orderDTOs.add(orderToOrderDTO(order));
+		}
+		
+		clientDTO.setOrders(orderDTOs);
+		
 		return clientDTO;
 	}
 	
 	public static Client clientDTOToClient( ClientDTO clientDTO) {
 		Client client = new Client();
+		Set<Order> orders = null;
 		client.setClient_id(clientDTO.getClient_id());
 		client.setClient_name(clientDTO.getClient_name());
 		client.setClient_lastname(clientDTO.getClient_lastname());
 		client.setAddress(clientDTO.getAddress());
 		client.setPhone_number(clientDTO.getPhone_number());
+		
+		for (OrderDTO orderDTO : clientDTO.getOrders()) {
+			orders.add(orderDTOToOrder(orderDTO));
+		}
+		
+		client.setOrders(orders);
+		
 		return client;
 	}
 	
@@ -78,36 +102,66 @@ public class DataMapper {
 	}
 	public static OrderDTO orderToOrderDTO(Order order) {
 		OrderDTO orderDTO = new OrderDTO();
+		Set<DetailDTO> detailsDTO = null;
 		orderDTO.setOrder_id(order.getOrder_id());
 		orderDTO.setFecha(order.getFecha());
 		orderDTO.setTotal_pay(order.getTotal_pay());
 		orderDTO.setClient(order.getClient());
+		orderDTO.setClient(order.getClient());
+		
+		for (Detail detail : order.getDetails()) {
+			detailsDTO.add(detailToDetailDTO(detail));
+		}
+		
+		orderDTO.setDetails(detailsDTO);
+		
 		return orderDTO;
 	}
 	public static Order orderDTOToOrder(OrderDTO orderDTO) {
 		Order order = new Order();
+		Set<Detail> details = null;
 		order.setOrder_id(orderDTO.getOrder_id());
 		order.setFecha(orderDTO.getFecha());
 		order.setTotal_pay(orderDTO.getTotal_pay());
 		order.setClient(orderDTO.getClient());
+		
+		for (DetailDTO detailDTO : orderDTO.getDetails()) {
+			details.add(detailDTOToDetail(detailDTO));
+		}
+		
+		order.setDetails(details);
+		
 		return order;
 	}
 	public static ProductDTO productToProductDTO(Product product) {
 		ProductDTO productDTO = new ProductDTO();
+		Set<DetailDTO> detailsDTO = null;
 		productDTO.setProduct_id(product.getProduct_id());
 		productDTO.setProduct_name(product.getProduct_name());
 		productDTO.setPrice(product.getPrice());
 		productDTO.setStock(product.getStock());
 		productDTO.setCategory(product.getCategory());
+		
+		for (Detail detail: product.getDetails()) {
+			detailsDTO.add(detailToDetailDTO(detail));
+		}
+		productDTO.setDetails(detailsDTO);
+		
 		return productDTO;
 	}
 	public static Product productDTOToProduct(ProductDTO productDTO) {
 		Product product = new Product();
+		Set<Detail> details = null;
 		product.setProduct_id(productDTO.getProduct_id());
 		product.setProduct_name(productDTO.getProduct_name());
 		product.setPrice(productDTO.getPrice());
 		product.setStock(productDTO.getStock());
 		product.setCategory(productDTO.getCategory());
+		
+		for (DetailDTO detailDTO: productDTO.getDetails()) {
+			details.add(detailDTOToDetail(detailDTO));
+		}
+		product.setDetails(details);
 		return product;
 	}
 	
