4e4bfdc3d2877c156d3d08dfdcc9bb8b370c6ccb
Miguel Linares <107733760+MikaNiatsu@users.noreply.github.com>
2024-09-05 15:32:51 -0500

    Lunh metod replace

--- src/main/java/co/edu/unbosque/ElectroShop/utils/CardPass.java
+++ src/main/java/co/edu/unbosque/ElectroShop/utils/CardPass.java
@@ -1,65 +1,34 @@
 package co.edu.unbosque.ElectroShop.utils;
 
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.squareup.okhttp.HttpUrl;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
+
 
 
 public class CardPass {
-    private static final OkHttpClient client = new OkHttpClient();
-    private static final String baseUrl = "https://data.handyapi.com/bin/\"";
-    private static final Gson gson = new Gson();
-
     /**
-     * Checks if the given card number is valid.
-     *
-     * @param card_number the card number to be validated
-     * @return true if the card number is valid, false otherwise
+     * Checks if a given card number is valid according to the Luhn formula.
+     * @param card_number the card number to check
+     * @return true if the card number is valid, false if not
      */
     public static boolean isValidCard(String card_number) {
-        HttpUrl url = HttpUrl.parse(baseUrl).newBuilder()
-                .addPathSegment(card_number)
-                .build();
+        int sum = 0;
+        boolean alternate = false;
 
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        try {
-            Response response = client.newCall(request).execute();
-            if (response.isSuccessful()) {
-                return parseResponse(response.body().string());
-            } else {
-                System.err.println("Respuesta no exitosa: " + response.code() + " " + response.message());
-                return false;
+        for (int i = card_number.length() - 1; i >= 0; i--) {
+            int n = Integer.parseInt(card_number.substring(i, i + 1));
+            if (alternate) {
+                n *= 2;
+                if (n > 9) {
+                    n -= 9;
+                }
             }
-        } catch (Exception e) {
-            System.err.println("Excepción durante la validación de la tarjeta: " + e.getMessage());
-            return false;
+            sum += n;
+            alternate = !alternate;
         }
+
+        return (sum % 10 == 0);
     }
 
-    /**
-     * Parse the response of the CardPass API, returning true if the card is valid,
-     * false otherwise.
-     *
-     * @param jsonResponse the response of the CardPass API
-     * @return true if the card is valid, false otherwise
-     */
-    private static boolean parseResponse(String jsonResponse) {
-        try {
-
-            JsonObject jsonObject = gson.fromJson(jsonResponse, JsonObject.class);
-            String status = jsonObject.get("Status").getAsString();
-            return "SUCCESS".equals(status);
-        } catch (Exception e) {
-            System.err.println("Excepción durante la validación de la tarjeta: " + e.getMessage());
-            return false;
-        }
-    }
 
 
 
